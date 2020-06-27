99af054fe080e9f4cf58d398abf9d3447379b1ee
SibirianNomad <SibirianNomad@gmail.com>
2020-06-27 12:46:11 +0700

    new project

--- index.php
+++ index.php
@@ -1,101 +1,50 @@
-<?session_start();?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title>WebStripe</title>
-<meta name='viewport' content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css" media="screen" >
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/style.css">
+<?
+require __DIR__.'/vendor/autoload.php';
 
-	
-</head>
-<body>
-    <div class='m-3 p-3'>
-        
-    <p class='text-right'>Евгений Курченко</p>
-    <h1 class='text-center xs'>ТЕСТОВОЕ ЗАДАНИЕ WEBSTRIPE</h1>
-    <div class='container h-50'>
-            <div id="skySlider" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#skySlider" data-slide-to="0" class="active"></li>
-                    <li data-target="#skySlider" data-slide-to="1"></li>
-                    <li data-target="#skySlider" data-slide-to="2"></li>
-                    <li data-target="#skySlider" data-slide-to="3"></li>
-                    <li data-target="#skySlider" data-slide-to="4"></li>
-                    <li data-target="#skySlider" data-slide-to="5"></li>
-                </ol>
-                <div class="carousel-inner text-center">
-                    <div class="carousel-item active">
-                    <img  class="w-50 h-25" decoding="auto"  srcset="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/51226445_385583958938758_4124171221060616685_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=RhpNz_6JxpkAX_MWvQN&amp;oh=1457da7986c587c3fde695567f71344b&amp;oe=5F17C2C7 640w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/51226445_385583958938758_4124171221060616685_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=RhpNz_6JxpkAX_MWvQN&amp;oh=492f29ec790058f32a941d881d462193&amp;oe=5F1821A4 750w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/51226445_385583958938758_4124171221060616685_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=RhpNz_6JxpkAX_MWvQN&amp;oh=492f29ec790058f32a941d881d462193&amp;oe=5F1821A4 1080w" src="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/51226445_385583958938758_4124171221060616685_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=RhpNz_6JxpkAX_MWvQN&amp;oh=492f29ec790058f32a941d881d462193&amp;oe=5F1821A4">
-                    </div>
-                    <div class="carousel-item">
-                    <img  class="w-50 h-25" decoding="auto"  srcset="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/44456694_268624977129296_2375744345501525626_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=108&amp;_nc_ohc=RRYcYSX8h1sAX-u0vKL&amp;oh=fd200d914cb1a304b443e4b1027c948d&amp;oe=5F15F62B 640w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/44456694_268624977129296_2375744345501525626_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=108&amp;_nc_ohc=RRYcYSX8h1sAX-u0vKL&amp;oh=77959445c7f22571c15cfd3c5909d452&amp;oe=5F159BD0 750w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/44456694_268624977129296_2375744345501525626_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=108&amp;_nc_ohc=RRYcYSX8h1sAX-u0vKL&amp;oh=77959445c7f22571c15cfd3c5909d452&amp;oe=5F159BD0 1080w" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/44456694_268624977129296_2375744345501525626_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=108&amp;_nc_ohc=RRYcYSX8h1sAX-u0vKL&amp;oh=77959445c7f22571c15cfd3c5909d452&amp;oe=5F159BD0">
-                    </div>
-                    <div class="carousel-item">
-                    <img  class="w-50 h-25" decoding="auto"  srcset="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/19761160_209217309602119_9008897063903035392_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=107&amp;_nc_ohc=lVYAbTNDXa8AX8Kdo8-&amp;oh=ab068bf9820a2931b1cf01118c5b0a3a&amp;oe=5F1812CB 640w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/19761160_209217309602119_9008897063903035392_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=107&amp;_nc_ohc=lVYAbTNDXa8AX8Kdo8-&amp;oh=bd931596308df7ecbe0a21cd34500738&amp;oe=5F16804B 750w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/19761160_209217309602119_9008897063903035392_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=107&amp;_nc_ohc=lVYAbTNDXa8AX8Kdo8-&amp;oh=9fdbcc3ea81d1c1f22783a568b54c77b&amp;oe=5F1650B0 1080w" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/19761160_209217309602119_9008897063903035392_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=107&amp;_nc_ohc=lVYAbTNDXa8AX8Kdo8-&amp;oh=9fdbcc3ea81d1c1f22783a568b54c77b&amp;oe=5F1650B0">
-                    </div>
-                    <div class="carousel-item">
-                    <img   class="w-50 h-25" decoding="auto"  srcset="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/42434657_1169730956501602_5546007552308781072_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=xam3I1WDBesAX-uDEAS&amp;oh=6660198483d5124d983a30c045b34d3b&amp;oe=5F152A3A 640w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/42434657_1169730956501602_5546007552308781072_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=xam3I1WDBesAX-uDEAS&amp;oh=c857a5dd446fdef74e782ff61dc51665&amp;oe=5F168B7E 750w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/42434657_1169730956501602_5546007552308781072_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=xam3I1WDBesAX-uDEAS&amp;oh=b0e0fc556b187aefa415bbca7cb10142&amp;oe=5F183F04 1080w" src="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/42434657_1169730956501602_5546007552308781072_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=xam3I1WDBesAX-uDEAS&amp;oh=b0e0fc556b187aefa415bbca7cb10142&amp;oe=5F183F04">
-                    </div>
-                    <div class="carousel-item">
-                    <img  class="w-50 h-25" decoding="auto" srcset="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.135.1080.1080a/s150x150/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=8d2922c09ef35a463a550833d25651c3&amp;oe=5F180A20 150w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.135.1080.1080a/s240x240/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=4ff3eb915bb06765df7015267c524902&amp;oe=5F17C01A 240w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.135.1080.1080a/s320x320/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=47b80c5110520b787a8cab431eeb65fd&amp;oe=5F16ED68 320w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.135.1080.1080a/s480x480/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=2b0d67a3f37147f3a01f75148e97c49f&amp;oe=5F1604A1 480w,https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=14e0ac3914c89827318e587b80cc6331&amp;oe=5F1678B0 640w" src="https://instagram.fhel3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/28427910_1208761362588226_7213802288400302080_n.jpg?_nc_ht=instagram.fhel3-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=UxtGZGAEmr8AX8cAkL6&amp;oh=14e0ac3914c89827318e587b80cc6331&amp;oe=5F1678B0">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="w-50 h-25" decoding="auto"  srcset="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/45493016_138383270472575_3413528900122553152_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=111&amp;_nc_ohc=Ah0gkMwehKAAX-B1wvF&amp;oh=e2b8a9ae81ecb68d541d0db84b460d65&amp;oe=5F1544A3 640w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/45493016_138383270472575_3413528900122553152_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=111&amp;_nc_ohc=Ah0gkMwehKAAX-B1wvF&amp;oh=4bde9fdff080dd939e2ee3a795b04f0c&amp;oe=5F176BA3 750w,https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/45493016_138383270472575_3413528900122553152_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=111&amp;_nc_ohc=Ah0gkMwehKAAX-B1wvF&amp;oh=d34ab6e888c2d1d9be0c0510e840dd50&amp;oe=5F15CF48 1080w" src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/45493016_138383270472575_3413528900122553152_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&amp;_nc_cat=111&amp;_nc_ohc=Ah0gkMwehKAAX-B1wvF&amp;oh=d34ab6e888c2d1d9be0c0510e840dd50&amp;oe=5F15CF48">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#skySlider" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#skySlider" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-         </div>
-    <div class='d-sm-flex mt-2'>
-        <form method='POST' action='action/first_form.php' id='first_form' enctype="multipart/form-data" class='text-center col-sm-6'>
-            <h4 class='text-center'>Форма</h1>
-            <input type='text' name='name' placeholder='Имя' class='form-control'>
-            <input type='text' name='email' placeholder='email' class='form-control mt-2'>
-            <input type='text' name='text' placeholder='Текстовая строка' class='form-control mt-2'>
-            <input type='number' name='num1' placeholder='Число 1' class='form-control mt-2'>
-            <input type='number' name='num2' placeholder='Число 2' class='form-control mt-2'>
-            <input type='file' id='fileUpload' name='fileUpload' class='form-control-file mt-2'>
-            <button type='submit' class='btn btn-secondary mt-2 ml-auto mr-auto'>Отправить</button>
-        </form>
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> gegreg<br>
+<b>Почта:</b> ergeg<br><br>
+<b>Сообщение:</b><br>ergegeg
+";
 
-        <form method='POST'  action='action/second_form.php'  id='second_form' class='col-sm-6 ml-2'>
-            <h4 class='text-center'>Результат</h1>
-            <button type='submit' class='btn btn-secondary mb-1'>Вывести</button>
-<? if(isset($_SESSION['name'])){ ?>
-            <p>Имя: <input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['name'])){echo $_SESSION['name'];} ?>"></p>
-            <p>Почта: <input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['email'])){echo $_SESSION['email'];} ?>"></p>
-            <p>Текстовая строка: <input type='text' disabled  class='border-0' value="<?php if(isset($_SESSION['text'])){echo $_SESSION['text'];} ?>"></p>
-            <p>Число 1: <input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['num1'])){echo $_SESSION['num1'];} ?>"></p>
-            <p>Число 2: <input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['num2'])){echo $_SESSION['num2'];} ?>"></p>
-            <p>Сумма:<input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['sum'])){echo $_SESSION['sum'];} ?>"></p>
-            <p><input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['merged'])){echo $_SESSION['merged'];} ?>"></p>
-            <p><input type='text' disabled class='border-0' value="<?php if(isset($_SESSION['length'])){echo $_SESSION['length'];} ?>"></p>
-            <p><?=$_SESSION['sum_column']?></p>
-<? } ?>
-        </form>
-    </div>
-    <div id='popup' style="display:none;" class='rounded'>
-			<div class='header-h1'>Ваш персонаж создан</div>
-    </div>
-    </div>
-    <div class='mt-2'>
-    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa591d96c135165a81d03f2b760dadf1803a5e49d90b83ca130e25ecab438aeaf&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true"></script>
-    </div>
-</body>
-</html>
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="js/jquery.fancybox-1.3.4.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.yahoo.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'ekurchenko@yahoo.com'; // Логин на почте
+    $mail->Password   = 'Faraon25'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('ekurchenko@yahoo.com', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('0491a@sibmail.com');  
+
+   
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+
+?>
